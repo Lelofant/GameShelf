7c58417bfa25289e417cdeda6e89416886991a45
Lelofant <libragainsboro@gmail.com>
2026-02-13 15:22:23 +0200

    Update Home controller with dashboard statistics

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,45 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GameShelf.Models;
+using GameShelf.Data;
 
 namespace GameShelf.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<Game> allGames = _context.Games.ToList();
+
+        ViewBag.TotalGames = allGames.Count;
+        ViewBag.CompletedGames = allGames.Count(g => g.IsCompleted);
+       
+
+        List<Platform> allPlatforms = _context.Platforms.ToList();
+        ViewBag.Platforms = allPlatforms.Count;
+
+        List<Genre> allGenres = _context.Genres.ToList();
+        ViewBag.Genres = allGenres.Count;
+
+        foreach (var game in allGames)
+        {
+            game.Platform = allPlatforms.FirstOrDefault(p => p.Id == game.PlatformId);
+            game.Genre = allGenres.FirstOrDefault(g => g.Id == game.GenreId);
+        }
+
+        List<Game> recentGames = allGames.OrderByDescending(g => g.Id).Take(5).ToList();
+
+        return View(allGames);
+
     }
 
     public IActionResult Privacy()
