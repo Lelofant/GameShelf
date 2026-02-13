04b8deef504d4b97871d4950f8a57b30781690b9
Lelofant <libragainsboro@gmail.com>
2026-02-13 15:21:39 +0200

    Add Game , Genre and Platfrom controllers with CRUD operations

---
+++ Controllers/GenreController.cs
@@ -0,0 +1,125 @@
+﻿using GameShelf.Data;
+using GameShelf.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShelf.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Genre> genres = _context.Genres.ToList();
+            return View(genres);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Genre genre = _context.Genres.Find(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Genres.Add(genre);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Genre genre = _context.Genres.Find(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Genres.Update(genre);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+        public IActionResult Delete(int? id)  
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Genre genre = _context.Genres.Find(id);
+
+            if(genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Genre genre = _context.Genres.Find(id);
+
+            if (genre != null)
+            { 
+            _context.Genres.Remove(genre);
+            _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/PlatformController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameShelf.Data;
+using GameShelf.Models;
+
+namespace GameShelf.Controllers
+{
+    public class PlatformController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlatformController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Platform> platforms = _context.Platforms.ToList();
+            return View(platforms);
+        }
+
+        public IActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Platform platform = _context.Platforms.Find(id);
+
+            if (platform == null)
+            {
+                return NotFound();
+            }
+
+            return View(platform);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Platform platform)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Platforms.Add(platform);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(platform);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Platform platform = _context.Platforms.Find(id);
+
+            if (platform == null)
+            {
+                return NotFound();
+            }
+
+            return View(platform);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id ,Platform platform)
+        {
+            if(id != platform.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Platforms.Update(platform);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(platform);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Platform platform = _context.Platforms.Find(id);
+            if (platform == null)
+            {
+                return NotFound();
+            }
+            return View(platform);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Platform platform = _context.Platforms.Find(id);
+
+            if (platform != null)
+            {
+                _context.Platforms.Remove(platform);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
